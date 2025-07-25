5afbc5417ef3973c51c52d2c4298c273b7bf8b65
felice2577 <felicecolella77@gmail.com>
2025-07-25 07:09:00 +0200

    Add files via upload
    Signed-off-by: felice2577 <felicecolella77@gmail.com>

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>🍳 Benvenuto su SafeCook</h1>
+      <p>Questa è la tua app di cucina interattiva!</p>
+    </div>
+  );
+}
